bc89a6c2161c1c34cb6d7884b6c2f890e0f5d5b7
Xenion1987 <39803750+Xenion1987@users.noreply.github.com>
2023-11-06 08:32:58 +0000

    remove pre-creating network

--- .devcontainer/scripts/initialize-command.sh
+++ .devcontainer/scripts/initialize-command.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 DOCKER_VOLUME_NAME="devcontainer-history"
-DOCKER_NETWORK_NAME="saltstack-playground"
 
 if ! docker volume inspect "${DOCKER_VOLUME_NAME}" &>/dev/null 2>&1; then
   echo -n "INITIALIZE COMMAND :: Creating Docker volume '${DOCKER_VOLUME_NAME}'... "
@@ -11,14 +10,6 @@ if ! docker volume inspect "${DOCKER_VOLUME_NAME}" &>/dev/null 2>&1; then
 else
   echo "INITIALIZE COMMAND :: Docker volume '${DOCKER_VOLUME_NAME}' already exists - no need to create."
 fi
-if [[ ! $(docker network ls -qf name=${DOCKER_NETWORK_NAME}) =~ [0-9a-z]{12} ]]; then
-  echo -n "INITIALIZE COMMAND :: Creating Docker network '${DOCKER_NETWORK_NAME}'... "
-  if docker network create "${DOCKER_NETWORK_NAME}" &>/dev/null; then
-    echo "done"
-  fi
-else
-  echo "INITIALIZE COMMAND :: Docker network '${DOCKER_NETWORK_NAME}' already exists - no need to create."
-fi
-### Build salt-playground
+### Build debian base image
+echo "INITIALIZE COMMAND :: Building Debian base image..."
 docker build -t local/debian-base -f ".devcontainer/Dockerfiles/Dockerfile-Debian-11-base" .
-# docker compose -f ".devcontainer/docker-compose.yml" build
